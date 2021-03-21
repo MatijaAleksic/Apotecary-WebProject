bd87ed23b6c8db8b76433823ff8a952467473e1e
Matija <matijaaleksic97@gmail.com>
2021-03-21 22:55:00 +0100

    osnovna komponenta uradjena i dodata dinamicka promena komponenta radi demonstracije zadataka

--- Apoteka/src/frontend/src/App.vue
+++ Apoteka/src/frontend/src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <HelloWorld />
+  <div>
+    <component v-bind:is="component"> </component>
+
+    <button v-on:click="component ='register-dermatologist'">Register Dermathologist</button>
+    <button>2.Komponenta</button>
+    <button>3.Komponenta</button>
+
+
+  </div>
+  <div>
+        <button v-on:click="component ='null'">Close</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import RegisterDermatologist from './components/RegisterDermatologist.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    //HelloWorld
+    'register-dermatologist' : RegisterDermatologist,
+  },
+
+  data(){
+    return{
+      component:null
+    }
   }
+
+  // methods: {
+  //   swapComponents : function(component)
+  //   {
+  //     this.currentComponent = component;
+  //   }
+  // }
 }
 </script>
 
---
+++ Apoteka/src/frontend/src/components/RegisterDermatologist.vue
@@ -0,0 +1,27 @@
+<template>
+  <form action="/action_page.php" method="get" target="_blank">
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname"><br><br>
+    <label for="lname">Last name:</label>
+    <input type="text" id="lname" name="lname"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'RegisterDermatologist',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    fetch("/api/messages/hello")
+      .then((response) => response.text())
+      .then((data) => {
+          this.msg = data;
+      });
+  }
+}
+</script>
