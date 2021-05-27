ba5fa6e203c8dfbb4293ae84d3a3497dd0b60d84
Matija <matijaaleksic97@gmail.com>
2021-05-27 13:28:55 +0200

    uradjen filterovanje za dermatologa i farmaceuta po svim parametrima

--- Apoteka/src/frontend/src/components/Tables/DermatologistTable.vue
+++ Apoteka/src/frontend/src/components/Tables/DermatologistTable.vue
@@ -3,17 +3,17 @@
 
     <table>
         <tr bgcolor='lightgrey'>
-            <th>ID</th>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Username</th> 
-            <th>email</th>           
-            <th>Address</th>
-            <th>City</th>
-            <th>Country</th>
-            <th>Phone</th>
-            <th>Start Hours</th>
-            <th>End Hours</th>
+            <th><div @click="sortBy('id')" class="sortBy">ID</div></th>
+            <th><div @click="sortBy('firstname')" class="sortBy">Firstname</div></th>
+            <th><div @click="sortBy('lastname')" class="sortBy">Lastname</div></th>
+            <th><div @click="sortBy('username')" class="sortBy">Username</div></th>
+            <th><div @click="sortBy('email')" class="sortBy">Email</div></th>
+            <th><div @click="sortBy('adress')" class="sortBy">Address</div></th>
+            <th><div @click="sortBy('city')" class="sortBy">City</div></th>
+            <th><div @click="sortBy('country')" class="sortBy">Country</div></th>
+            <th><div @click="sortBy('phone')" class="sortBy">Phone</div></th>
+            <th><div @click="sortBy('startHours')" class="sortBy">Start Hours</div></th>
+            <th><div @click="sortBy('endHours')" class="sortBy">End Hours</div></th>
         </tr>
 
         <tr v-for="derma in dermatologists"  v-bind:key="derma.id"> 
@@ -72,6 +72,10 @@ export default {
             if(this.mode == 'BROWSE'){
                 this.selectedDermatologist = dermatologist;
             }
+        },
+
+        sortBy(prop) {
+            this.dermatologists.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
         },
 
         deleteDermatologist(identification){
